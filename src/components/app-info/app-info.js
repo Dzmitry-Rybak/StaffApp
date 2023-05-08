c914979b0ccb700adcb0c5020cf6d2f4433bc08f
Dzmitry Rybak <nakadisa@icloud.com>
2023-05-08 17:53:26 +0200

    start first React Application named 'Staff App'

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,15 @@
+
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Employee management at the company "Horns and hooves"</h1>
+            <h2>The total number of employees: </h2>
+            <h2>The bonus will be awarded to: </h2>
+
+        </div>
+    )
+}
+
+export default AppInfo;
